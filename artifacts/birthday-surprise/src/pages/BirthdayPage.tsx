8e974f0d75a3d295f9e8d99e3e5cdda5667e6d5e
jusfallin <deepanb0309@gmail.com>
2026-08-12 15:52:46 +0530

    Switch birthday page to Bhavanika experience

--- artifacts/birthday-surprise/src/pages/BirthdayPage.tsx
+++ artifacts/birthday-surprise/src/pages/BirthdayPage.tsx
@@ -1,29 +1,10 @@
-import React, { useEffect } from 'react';
-import Hero from '@/components/Hero';
-import IntroMessage from '@/components/IntroMessage';
-import PhotoQuote from '@/components/PhotoQuote';
-import InteractiveGift from '@/components/InteractiveGift';
-import HeartfeltMessage from '@/components/HeartfeltMessage';
-import FinalWish from '@/components/FinalWish';
-import SparklesOverlay from '@/components/SparklesOverlay';
+import { useEffect } from 'react';
+import BhavanikaExperience from '@/components/BhavanikaExperience';
 
 export default function BirthdayPage() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  return (
-    <div className="min-h-[100dvh] w-full bg-background text-foreground relative overflow-hidden selection:bg-primary/30">
-      <SparklesOverlay />
-      
-      <main className="relative z-10 flex flex-col items-center">
-        <Hero />
-        <IntroMessage />
-        <InteractiveGift />
-        <PhotoQuote />
-        <HeartfeltMessage />
-        <FinalWish />
-      </main>
-    </div>
-  );
+  return <BhavanikaExperience />;
 }
