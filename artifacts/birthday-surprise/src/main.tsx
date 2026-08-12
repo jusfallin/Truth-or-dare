67c16ed1ef34870217a8d743d7c3c73a46516372
jusfallin <deepanb0309@gmail.com>
2026-08-12 19:26:06 +0530

    Load Bhavanika mobile and entrance polish

--- artifacts/birthday-surprise/src/main.tsx
+++ artifacts/birthday-surprise/src/main.tsx
@@ -5,5 +5,6 @@ import App from './App';
 import './index.css';
 import './bhavanika.css';
 import './bhavanika-overrides.css';
+import './bhavanika-mobile-fix.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
